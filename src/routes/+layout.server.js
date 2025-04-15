8aff75b6847c9d6943059e36562eb5029c4fa1e3
KolyStudio <kolystudio@outlook.com>
2025-04-15 18:17:31 +0400

    first commit

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,9 +1,5 @@
 /** @type {import('./$types').LayoutServerLoad} */
 export function load({ request }) {
-  // Récupérer le User-Agent pour l'utiliser dans les composants si nécessaire
-  const userAgent = request.headers.get('user-agent') || '';
-  
   return {
-    userAgent
   };
 }
