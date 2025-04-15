f98d85554e1577f9d94590af89362bca29c9bdd3
KolyStudio <kolystudio@outlook.com>
2025-04-15 17:20:15 +0400

    first commit

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -6,7 +6,7 @@ export function load() {
     if (
       /FBAN|FBAV|Twitter|TwitterAndroid|Instagram|LinkedIn|Pinterest|MicroMessenger|WhatsApp|Snapchat|Line\/|KAKAOTALK/.test(ua)
     ) {
-      window.location.href = "x-safari-https://testtest-one-beta.vercel.app/";
+      window.location.href = "x-safari-https://testi-4c8n.vercel.app/";
       return new Promise(() => {}); // Bloque le chargement pendant la redirection
     }
   }
